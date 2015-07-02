89a708f2e6f45e166b0b014d8ba5ba5277e8f089
Vikas Kumar <vrnvikas1994@gmail.com>
2015-07-02 14:48:30 +0530

    deleted sliding tab layout and strip

--- app/src/main/java/com/teamdroid/algoninja/ArtWorkActivity.java
+++ app/src/main/java/com/teamdroid/algoninja/ArtWorkActivity.java
@@ -1,7 +1,5 @@
 package com.teamdroid.algoninja;
 
-import android.app.Activity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,7 +9,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 /**
  * Created by Murali on 24-06-2015.
