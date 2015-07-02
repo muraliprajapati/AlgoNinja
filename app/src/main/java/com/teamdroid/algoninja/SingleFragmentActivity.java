7faa9a5e861531d08acc798118c6b02d93bee171
Vikas Kumar <vrnvikas1994@gmail.com>
2015-07-02 14:46:21 +0530

    deleted sliding tab layout and strip

--- app/src/main/java/com/teamdroid/algoninja/SingleFragmentActivity.java
+++ app/src/main/java/com/teamdroid/algoninja/SingleFragmentActivity.java
@@ -3,7 +3,6 @@ package com.teamdroid.algoninja;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,8 +10,10 @@ import android.support.v7.widget.Toolbar;
  * Created by Murali on 10-04-2015.
  */
 public abstract class SingleFragmentActivity extends AppCompatActivity {
-    protected abstract Fragment createFragment();
     Toolbar toolbar;
+
+    protected abstract Fragment createFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
