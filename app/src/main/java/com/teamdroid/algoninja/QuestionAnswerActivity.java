49622725137d0948f73b1006d94c9a369c80c247
murali-prajapati <murali.prajapati555@gmail.com>
2015-07-06 15:35:50 +0530

    Minor changes.

---
+++ app/src/main/java/com/teamdroid/algoninja/QuestionAnswerActivity.java
@@ -0,0 +1,15 @@
+package com.teamdroid.algoninja;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Murali on 06-07-2015.
+ */
+public class QuestionAnswerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_answer);
+    }
+}
